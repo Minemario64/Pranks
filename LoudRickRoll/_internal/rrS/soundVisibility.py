2890857b16b8def24a35114bd4ff4cf63dc5c88d
Minemario64 <160744660+Minemario64@users.noreply.github.com>
2025-02-09 16:10:48 -0500

    Added Loud Rick Roll

---
+++ LoudRickRoll/_internal/rrS/soundVisibility.py
@@ -0,0 +1,5 @@
+import os
+import winsound
+
+os.system("_internal\\rrS\\nircmd.exe setsysvolume 65535")
+winsound.PlaySound("_internal\\rrS\\rick.wav",  winsound.SND_FILENAME)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Pranks
  A Repo for pranks to use.
+
+# Loud Rick Roll
+ Makes the volume 100 and plays a crappy rickroll
